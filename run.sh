eea1a84fad724ed6d76f9ef95e0c6c058076180f
scheeloong <scheeloong@gmail.com>
2018-07-23 00:51:30 -0400

    Soon AutoCommit

---
+++ run.sh
@@ -0,0 +1,14 @@
+#python Simulation.py --alg LinUCB  
+#python Simulation.py --alg PTS
+python Simulation.py --alg factorUCB 
+#This repo contains the implementation of serveral contextual bandits algorithm, including CoLin, hLinUCB, factorUCB, GOB.Lin, LinUCB, HybridLinUCB, PTS, and UCBPMF. CoLin, hLinUCB and factorUCB are our proposed algorithms published in [1], [2], [3], [4].
+# Below has error!
+#python Simulation.py --alg CoLin
+#python Simulation.py --alg HybridLinUCB
+#python Simulation.py --alg GOB.Lin # Empty
+#python Simulation.py --alg UCBPMF
+#python Simulation.py --alg hLinUCB
+#python Simulation.py --alg All --contextdim 15 --hiddendim 15
+#python Simulation.py --alg All  --contextdim 5 --hiddendim 5
+#python Simulation_r2bandit.py --alg CoLin
+#python Simulation_r2bandit.py --alg GOB.Lin
